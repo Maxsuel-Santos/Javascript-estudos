75f99e3220cc8b4e79c461fa474a1dbebd35851a
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-04-24 20:14:15 -0300

    .

---
+++ estudos/aula01/script.js
@@ -0,0 +1,7 @@
+console.log("MENSAGEM..."); // Registro/Mensagem no console do browser
+//
+console.warn("AVISO..."); // Aviso no console do browser
+//
+console.error("ERRO..."); // Erro no console do browser
+//
+console.error("INFORMAÇÃO..."); // Informação no console do browser
