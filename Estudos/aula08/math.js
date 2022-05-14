f70ad5b392b06cfbc009fefd761214ad3c25dccb
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-05-14 20:22:47 -0300

    Update math.js

--- Estudos/aula08/math.js
+++ Estudos/aula08/math.js
@@ -2,8 +2,40 @@
 
 => OBJETO MATH
 
-* 
+* Math.floor() => Arredonda um valor para baixo. Ex: 9.54578 -> 9
+
+* Math.ceil() => Arredonda um valor para cima. Ex: 9.54578 -> 10
+
+* Math.round() => Arredonda um valor para o inteiro mais próximo. Ex: 9.54578 -> 10. 9.4765 -> 9. Se passa da metade arredonda pra cima. Se não passa da metade, arredonda para baixo. Se está no meio termo, arredonda para cima.
+
+* Math.max() => Retorna o maior valor. Pode ser usado variáveis ou números dentro dos parênteses que será retornado o maior valor.
+
+* Math.min() => Retorna o menor valor. Pode ser usado variáveis ou números dentro dos parênteses que será retornado o menor valor.
+
+* Math.random() => Gera valores aleatórios entre 0 e 1, sendo que o 1 não será incluído. Ex: 0.4682375691797181797151671.
+
+* Math.
+
+* Math.
 
 */
 
 
+let n = 9.5;
+console.log(Math.round(n));
+
+//
+
+let n1 = 51;
+let n2 = 925;
+let n3 = 08;
+let n4 = 48498;
+console.log(Math.max(n1, n2, n3, n4));
+console.log(Math.min(n1, n2, n3, n4));
+
+//
+
+const alet = Math.random() * 10; // 0 a 10
+console.log(alet);
+
+//
