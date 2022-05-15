4654cdf95bc140baef52f661bd617c5119e93c71
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-05-14 21:01:05 -0300

    Update math.js

--- Estudos/aula08/math.js
+++ Estudos/aula08/math.js
@@ -12,14 +12,21 @@
 
 * Math.min() => Retorna o menor valor. Pode ser usado variáveis ou números dentro dos parênteses que será retornado o menor valor.
 
-* Math.random() => Gera valores aleatórios entre 0 e 1, sendo que o 1 não será incluído. Ex: 0.4682375691797181797151671.
+* Math.random() => Gera valores aleatórios entre 0 e 1, sendo que o 0 e 1 não será incluído, ou seja, será retornado do 0.01 até o 0.99. Ex: 0.4682375691797181797151671.
 
-* Math.
+* Math.PI => Retorna o valor de pi (3.141592...).
 
-* Math.
+* Math.pow() => Potenciação. Ex: Math.pow(base, expoente);. 
+OBS: Existem o operador de potência ** no JavaScript, então fica a seu critério usar o pow() ou não. Não há tanta necessidade se já tem um operador pra isso.
+
+* Math.sqrt() => Retorna a raíz quadrado de um número/variável.
+OBS: Você pode obter a raíz quadrada de outra forma:
+    -> variavel ** .5
+    -> variavel ** (1/2)
+
+OBS: No JS a divisão por zero (0) é válida, gerando um tipo infinity como resultado, ou seja, infinito, uma vez que o resultado é enooooooorrrmeeee. Fique atento(a)!
 
 */
-
 
 let n = 9.5;
 console.log(Math.round(n));
@@ -35,7 +42,10 @@ console.log(Math.min(n1, n2, n3, n4));
 
 //
 
-const alet = Math.random() * 10; // 0 a 10
-console.log(alet);
-
-//
+//for(let i = 0; i < 100; i++) {
+    const alet = Math.random() * 11; // 0 a 10
+    // Pode por:  Math.round(Math.random() * 11);
+    console.log(alet);
+    console.log(alet.toFixed(0)); // Sem casas decimais, porém ocorre arredondamento automático (Math.round();).
+    console.log(Math.round(alet));
+//}
