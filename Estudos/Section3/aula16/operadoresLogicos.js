3d5580e87ada47acd7176f593c032869bf1d7062
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-05-21 15:02:06 -0300

    .

---
+++ Estudos/Section3/aula16/operadoresLogicos.js
@@ -0,0 +1,11 @@
+/* 
+
+OPERADORES LÓGICOS
+
+&& => AND => E => Retorna true quando todas as espressões são verdadeiras. Retorna false quando uma ou mais expressão(ões) são falsas.
+
+|| => OR => OU => Retorna true quando uma ou mais espressões são verdadeira(s). Só retorna false quando todas as expressões forem falsas.
+
+! => NOT => NÃO => Negação, inverte o resultado, por ex: !true é igual a false, pois tá negando o verdadeiro, se você nega algo verdadeiro, logo é false. !false é igual a true, pois se você nega algo falso, logo é verdadeiro.
+
+*/
