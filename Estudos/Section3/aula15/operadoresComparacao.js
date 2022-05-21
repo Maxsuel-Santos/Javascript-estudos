82345a379eb9df2de18e0d8ecf025e8ad9c062a8
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-05-21 14:26:53 -0300

    .

---
+++ Estudos/Section3/aula15/operadoresComparacao.js
@@ -0,0 +1,23 @@
+/* 
+
+OPERADORES DE COMPARAÇÃO
+
+> Maior que
+
+> Menor que
+
+>= Maior que ou igual a
+
+<= Menor que ou igual a
+
+== Igualdade (valor) ***
+
+=== Igualdade estrita (valor e tipo)
+
+!= Diferente (valor) ***
+
+!== Diferente estrito (valor e tipo)
+
+* OBS: Use sempre os operadore === e !===. Se você compara com == se 10 é igual a "10" retornará verdadeiro. Porém, 10 é diferente de "10", eles tem tipos primitivos diferentes, no caso number e string. Se usar o === ele retornará falso, pois os tipos number e string são diferentes, opostos. O mesmo valoe para o !== em relação ao !=, use o !== que retornará se proposições são diferentes em valor e tipo primitivo. Se por 10 é diferente de "10" usando o != retornará false, sendo que é true, pois se retorna false, significa que 10 e "10" são iguais. Quando retorna true significa que 10 e "10" são diferentes, usando o !==.
+
+*/
