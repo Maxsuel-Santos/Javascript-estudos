782b6b4b26caccee49fe53c6c6c05c1db80abbc7
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-05-27 21:01:40 -0300

    .

--- Estudos/Section3/aula19/src/js/app.js
+++ Estudos/Section3/aula19/src/js/app.js
@@ -1,3 +1,5 @@
+// IMC = peso / altura ** 2 ou IMC = peso / altura * altura 
+
 const peso = document.querySelector("#peso");
 const altura = document.querySelector("#altura");
 const result = document.querySelector(".result");
