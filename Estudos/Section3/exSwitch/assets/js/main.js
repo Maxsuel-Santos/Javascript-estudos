adc2eb1979a15013829ca89a3e32f605fc4133f3
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-09-14 16:23:04 -0300

    Update main.js

--- Estudos/Section3/exSwitch/assets/js/main.js
+++ Estudos/Section3/exSwitch/assets/js/main.js
@@ -99,9 +99,5 @@ function criaData(data) {
 
 const h1 = document.querySelector('.container h1');
 const data = new Date();
-const opcoes = {
-    dataStyle: 'full',
-    timeStyle: 'short'
-};
 
-h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
+h1.innerHTML = data.toLocaleDateString('pt-BR', {dateStyle: 'full', timeStyle: 'short'});
